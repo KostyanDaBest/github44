824714c31560d5429c76f23969909244e066edee
kosty <round.mc@yandex.by>
2023-11-16 13:01:14 -0500

    new

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class Main {
-}
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] arr = new int[10];
+        int x = 0;
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt();
+            if (i == 0) x++;
+        }
+            if (x == 0) {
+                System.out.println(" нет нулевых значений ");
+            }
+            else {
+                System.out.print(" нулевые значения : " + x );
+            }
+            }
+        }
